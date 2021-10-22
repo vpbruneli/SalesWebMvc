3e908418bd3cc704f9a9ca2c5b41247d89134643
vpbruneli <vpbruneli92@gmail.com>
2021-10-22 10:22:57 -0300

    Seeding Service

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,7 +1,6 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-
 
 namespace SalesWebMvc.Models
 {
@@ -9,23 +8,25 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
 
-        public Department() {
-             }
+        public Department()
+        {
+        }
 
-        public Department(int id, string name, ICollection<Seller> sellers)
+        public Department(int id, string name)
         {
             Id = id;
             Name = name;
-            
         }
-        public void AddSeller(Seller seller) {
+
+        public void AddSeller(Seller seller)
+        {
             Sellers.Add(seller);
         }
 
-        public double TotalSales(DateTime initial, DateTime final) {
+        public double TotalSales(DateTime initial, DateTime final)
+        {
             return Sellers.Sum(seller => seller.TotalSales(initial, final));
         }
     }
